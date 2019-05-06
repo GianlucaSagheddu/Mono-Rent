f2eff9a8df3f6392e4df975166b718f49524d986
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-05-06 11:36:01 +0000

    Registrazione

--- Mono-Rent/src/app/app.component.ts
+++ Mono-Rent/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Mono-Rent';
+   data: Object;
+
+   o :Observable<Object>;
+   constructor(public http: HttpClient) {}
+   makeCompactPost(Nome: HTMLInputElement, Cognome: HTMLInputElement, Usr: HTMLInputElement, Pass: HTMLInputElement, DataN: HTMLInputElement): boolean {
+
+   this.http
+     .post(' ',
+       JSON.stringify({
+         
+       })
+     )
+     .subscribe(data => {
+       this.data = data;
+
+     });
+     return false;
+ }
+
 }
