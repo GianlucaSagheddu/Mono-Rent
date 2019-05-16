5bcfeadca47510c01eec499d91fd59175d33af0d
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-05-16 17:34:29 +0000

    scootering

--- Mono-Rent/src/app/scootering/scootering.component.ts
+++ Mono-Rent/src/app/scootering/scootering.component.ts
@@ -1,13 +1,39 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient, HttpHeaders, HttpParams  } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+import { CookieService } from 'ngx-cookie-service';  //Cookie module
+import { Offerta } from './Offerta.model';
+
 @Component({
   selector: 'app-scootering',
   templateUrl: './scootering.component.html',
   styleUrls: ['./scootering.component.css']
 })
 export class ScooteringComponent implements OnInit {
+  o :Observable<Offerta[]>;
+  Offerte: Offerta[] = [];
+  constructor(public http: HttpClient, private cookieService: CookieService) { }
 
-  constructor() { }
+
+  offri(LatI: HTMLInputElement, LongI: HTMLInputElement, LatF: HTMLInputElement, LongF: HTMLInputElement, Data: HTMLInputElement):boolean{
+    return false;
+  }
+
+  carica(): boolean{
+    this.o = this.http.get<Offerta[]>('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/visuOff');
+      this.o.subscribe(data => {
+          for(var i = 0; i < data.length; i++){
+            this.Offerte.push(new Offerta(data[i].Nome, data[i].Cognome, data[i].Indirizzo, data[i].Telefono, data[i].Email, data[i].Data, data[i].Ora));
+
+            }
+
+        });
+
+
+    return false;
+  }
 
   ngOnInit() {
   }
