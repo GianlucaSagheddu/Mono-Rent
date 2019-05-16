8ea481e632db2b27d2664ae6c887be7e1f132cb8
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-05-16 18:02:35 +0000

    scootering

--- Mono-Rent/src/app/scootering/scootering.component.ts
+++ Mono-Rent/src/app/scootering/scootering.component.ts
@@ -13,11 +13,45 @@ import { Offerta } from './Offerta.model';
 })
 export class ScooteringComponent implements OnInit {
   o :Observable<Offerta[]>;
+  data: Object;
   Offerte: Offerta[] = [];
   constructor(public http: HttpClient, private cookieService: CookieService) { }
 
 
-  offri(LatI: HTMLInputElement, LongI: HTMLInputElement, LatF: HTMLInputElement, LongF: HTMLInputElement, Data: HTMLInputElement):boolean{
+  offri(IdMezzo: HTMLInputElement, LatI: HTMLInputElement, LongI: HTMLInputElement, LatF: HTMLInputElement, LongF: HTMLInputElement, Data: HTMLInputElement):boolean{
+    const headers = new HttpHeaders({
+          'Content-Type': 'application/x-www-form-urlencoded'
+
+        });
+
+
+        const params = new HttpParams()
+          .set('IdMezzo', IdMezzo.value)
+          .set('Data', Data.value)
+          .set('CoordI', LatI.value +", " +LongI.value)
+          .set('CoordF', LatF.value +", " + LongF.value)
+          .set('idUtente', this.cookieService.get('ID'));
+
+          const options = {
+          headers,
+          params,
+          withCredentials: false
+        };
+
+        var parameter = JSON.stringify({
+            idMezzo: IdMezzo.value,
+            Data: Data.value,
+            CoordI: [LatI.value, LongI.value],
+            CoordF: [LatF.value, LongF.value],
+            idUtente: this.cookieService.get('ID')
+        });
+
+        this.http.post('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/prenotaS',null, options  )
+          .subscribe(data => {
+            this.data = data;
+
+
+        });
     return false;
   }
 
