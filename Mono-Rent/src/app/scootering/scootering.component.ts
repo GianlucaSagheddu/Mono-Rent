4b39268872249a953f3c21d9293ad39b04652ed6
GianlucaSagheddu <gianlucasagheddu@gmail.com>
2019-05-17 16:37:06 +0000

    partecipa

--- Mono-Rent/src/app/scootering/scootering.component.ts
+++ Mono-Rent/src/app/scootering/scootering.component.ts
@@ -20,52 +20,84 @@ export class ScooteringComponent implements OnInit {
 
   offri(IdMezzo: HTMLInputElement, LatI: HTMLInputElement, LongI: HTMLInputElement, LatF: HTMLInputElement, LongF: HTMLInputElement, Data: HTMLInputElement):boolean{
     const headers = new HttpHeaders({
-          'Content-Type': 'application/x-www-form-urlencoded'
+      'Content-Type': 'application/x-www-form-urlencoded'
 
-        });
-
-
-        const params = new HttpParams()
-          .set('IdMezzo', IdMezzo.value)
-          .set('Data', Data.value)
-          .set('CoordI', LatI.value +", " +LongI.value)
-          .set('CoordF', LatF.value +", " + LongF.value)
-          .set('idUtente', this.cookieService.get('ID'));
-
-          const options = {
-          headers,
-          params,
-          withCredentials: false
-        };
-
-        var parameter = JSON.stringify({
-            idMezzo: IdMezzo.value,
-            Data: Data.value,
-            CoordI: [LatI.value, LongI.value],
-            CoordF: [LatF.value, LongF.value],
-            idUtente: this.cookieService.get('ID')
-        });
-
-        this.http.post('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/prenotaS',null, options  )
-          .subscribe(data => {
-            this.data = data;
+    });
 
 
-        });
+    const params = new HttpParams()
+      .set('IdMezzo', IdMezzo.value)
+      .set('Data', Data.value)
+      .set('CoordI', LatI.value +", " +LongI.value)
+      .set('CoordF', LatF.value +", " + LongF.value)
+      .set('idUtente', this.cookieService.get('ID'));
+
+      const options = {
+      headers,
+      params,
+      withCredentials: false
+    };
+
+    var parameter = JSON.stringify({
+        idMezzo: IdMezzo.value,
+        Data: Data.value,
+        CoordI: [LatI.value, LongI.value],
+        CoordF: [LatF.value, LongF.value],
+        idUtente: this.cookieService.get('ID')
+    });
+
+    this.http.post('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/prenotaS',null, options  )
+      .subscribe(data => {
+        this.data = data;
+
+
+    });
     return false;
   }
 
   carica(): boolean{
     this.o = this.http.get<Offerta[]>('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/visuOff');
-      this.o.subscribe(data => {
-          for(var i = 0; i < data.length; i++){
-            this.Offerte.push(new Offerta(data[i].IdProponente, data[i].IdPartecipante, data[i].Id, data[i].Percorso, data[i].Data));
+    this.o.subscribe(data => {
+      for(var i = 0; i < data.length; i++){
+        this.Offerte.push(new Offerta(data[i].IdProponente, data[i].IdPartecipante, data[i].Id, data[i].Percorso, data[i].Data));
+      }
 
-          }
+    });
+    return false;
+  }
 
-        });
 
 
+
+
+  partecipa(IdOfferta: number): boolean {
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/x-www-form-urlencoded'
+
+    });
+
+
+    const params = new HttpParams()
+      .set('idRichiesta', IdOfferta.toString())
+      .set('idUtente', this.cookieService.get('ID'));
+
+    const options = {
+      headers,
+      params,
+      withCredentials: false
+    };
+
+    var parameter = JSON.stringify({
+      idRichiesta: IdOfferta,
+      idUtente: this.cookieService.get('ID')
+    });
+
+    this.http.post('https://3000-d670e502-c231-409e-b2f8-68e3b042a9da.ws-eu0.gitpod.io/partecipaS',null, options  )
+      .subscribe(data => {
+        this.data = data;
+
+
+    });
     return false;
   }
 
